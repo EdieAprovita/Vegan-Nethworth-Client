2878d0cd778b583fe437a7a2643987bdd0cbd1ab
Edie-Aprovita <eacm316@gmai.com>
2021-04-15 18:26:51 -0500

    Adding reducers to the store

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,10 +2,16 @@ import { createStore, combineReducers, applyMiddleware } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import thunk from 'redux-thunk'
 import { userLoginReducer, userRegisterReducer } from './authDucks'
+import { postReducer } from './postDucks'
+import { profileReducer } from './profileDucks'
+import { alertReducer } from './alertDucks'
 
 const rootReducer = combineReducers({
-	userLogin:userLoginReducer,
-	userRegister:userRegisterReducer
+	userLogin: userLoginReducer,
+	userRegister: userRegisterReducer,
+	userPost: postReducer,
+	userProfile: profileReducer,
+	profileAlert: alertReducer,
 })
 
 const userInfoFromStorage = localStorage.getItem('userInfo')
